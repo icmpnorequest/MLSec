dd41accfb5963e06e72a31b75858f404920af52a
icmpnorequest <sculyt10@gmail.com>
2019-09-18 10:31:56 +0800

    Complete target model training

--- Membership_Inference/Experiment_CIFAR10.py
+++ Membership_Inference/Experiment_CIFAR10.py
@@ -17,6 +17,8 @@ import torch.nn as nn
 from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 import torch.optim as optim
+from torch.utils.data import Subset
+
 
 from Membership_Inference.nn_model import *
 
@@ -26,7 +28,7 @@ dataset_path = "../data"
 
 
 # Hyper-parameters
-num_epochs = 5
+num_epochs =3
 batch_size = 100
 learning_rate = 0.001
 num_workers = 2
@@ -68,7 +70,7 @@ def calc_mlleaks_cnn_size(size):
     return out
 
 
-def cifar10_loader(data_path, batch_size, num_workers):
+def cifar10_loader(data_path, batch_size, num_workers, sampler):
     """
     It is a function to load CIFAR-10 Data Loader.
     :param data_path: path of the dataset
@@ -90,28 +92,13 @@ def cifar10_loader(data_path, batch_size, num_workers):
                                                 transform=transform)
 
     # Data Loader
-    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers)
+    train_loader = torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=batch_size, num_workers=num_workers, sampler=sampler)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
 
     return train_loader, test_loader
 
 
 # train_loader, test_loader = cifar10_loader(dataset_path, batch_size, num_workers)
-
-def rm_array_comma(len, ndarray):
-    """
-    It is a function to remove comma in an array
-    :param ndarray: np.ndarray
-    :return: array without comma
-    """
-    for _ in range(len):
-        for i in ndarray:
-            if "," in i:
-                i = i.replace(",", "")
-    return ndarray
-
-
-
 
 
 def ndarray_to_tensor(ndarray):
@@ -120,7 +107,7 @@ def ndarray_to_tensor(ndarray):
     :param ndarray: np.ndarray
     :return: tensor object
     """
-    out = torch.from_numpy(ndarray)
+    out = torch.Tensor(list(ndarray))
     return out
 
 
@@ -145,7 +132,38 @@ def form_dataloader(tensor_dataset, batch_size, num_workers):
     return data_loader
 
 
-def train_target_model(ndarray_path, trainset_size, net, num_epochs, criterion, optimizer, device):
+def CIFAR10_transform():
+    """
+    It is a function to transform picture of CIFAR-10.
+    :return: transform
+    """
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    return transform
+
+
+
+def load_CIFAR10_dataset(dataset_path):
+    """
+    It is a function to load CIFAR-10 dataset
+    :param dataset_path: path of dataset
+    :return: train_dataset, test_dataset
+    """
+    transform = CIFAR10_transform()
+    train_dataset = torchvision.datasets.CIFAR10(root=dataset_path,
+                                                 download=True,
+                                                 train=True,
+                                                 transform=transform)
+    test_dataset = torchvision.datasets.CIFAR10(root=dataset_path,
+                                                download=True,
+                                                train=False,
+                                                transform=transform)
+    return train_dataset, test_dataset
+
+
+
+
+def train_target_model(ndarray_path, trainset_size, net, num_epochs, criterion, optimizer, device, dataset_path):
     """
     It is a function to train the target model
     :param net: neural network
@@ -166,41 +184,18 @@ def train_target_model(ndarray_path, trainset_size, net, num_epochs, criterion, 
     X = trainset_array[:, 0]            # Data, shape = (trainset_size, )
     y = trainset_array[:, 1]            # Label, shape = (trainset_size, )
 
-
-    test = X
-
-
-    # test1 = rm_array_comma(len(test), test)
-
-
-
-    test1 = np.frompyfunc(lambda x: x.replace(',', ''), 1, 1)(test).astype(float)
-    print(test1)
-
-
-    # test1 = torch.Tensor(list(test.values))
-
-    # print(test1)
-
-
-    # print(X)
-    # test = np.array([v.replace(',', '') for v in X], dtype=np.float32)
-    # print(test.dtype)
-
-    # test = np.core.defchararray.replace(X.astype(np.unicode_), ',', '').astype(np.float)
-    # print(test)
-
     # 3. ndarray -> tensor
-    # X_tensor = ndarray_to_tensor(ndarray=X)
-    # y_tensor = ndarray_to_tensor(ndarray=y)
+    X_tensor = ndarray_to_tensor(ndarray=X)
+    # X_tensor.size() =  torch.Size([2500, 32, 32, 3])
+    # X_tensor[0].size() =  torch.Size([32, 32, 3])
+    y_tensor = ndarray_to_tensor(ndarray=y).long()
+    # y_tensor.size() =  torch.Size([2500])
 
-    # X_tensor = torch.from_numpy(test).to(float)
+    # Reshape X_tensor: [batch_size, channel, height, width].
+    X_tensor = X_tensor.reshape([trainset_size, 3, 32, 32])
+    # X_tensor_test.size() =  torch.Size([trainset_size, 3, 32, 32])
+    # X_tensor_test[0].size() =  torch.Size([3, 32, 32])
 
-
-    # print(X_tensor)
-    print("\n")
-    # print(y_tensor)
-    """
     # 4. tensor -> dataset
     target_train_dataset = form_dataset(X_tensor=X_tensor, y_tensor=y_tensor)
 
@@ -209,15 +204,54 @@ def train_target_model(ndarray_path, trainset_size, net, num_epochs, criterion, 
                                           batch_size=batch_size,
                                           num_workers=num_workers)
 
+    # 5.1 Use API: torch.utils.data import Subset
+    subset_indices = list(range(trainset_size))
+    train_dataset, test_dataset = load_CIFAR10_dataset(dataset_path=dataset_path)
+    target_train_subset = Subset(train_dataset, indices=subset_indices)
+    target_train_subloader = DataLoader(target_train_subset, batch_size, shuffle=True)
+
     # 6. train
     train(net=net,
-          train_loader=target_train_loader,
+          train_loader=target_train_loader,     # if according to 5.1, change train_loader = target_train_subloader
           num_epochs=num_epochs,
           criterion=criterion,
           optimizer=optimizer,
           device=device)
-    """
 
+
+def bug_recorder():
+    """
+    It is a function to record bugs.
+    """
+    bug = "RuntimeError: Given groups=1, weight of size 6 3 5 5, " \
+          "expected input[100, 32, 32, 3] to have 3 channels, " \
+          "but got 32 channels instead"
+
+    return None
+
+
+def result_recorder():
+    """
+    It is a function to record results.
+    """
+    result = """ Custom Dataset
+    Epoch [1/3], Step [5/25], Loss: 2.3024
+    Epoch [1/3], Step [10/25], Loss: 2.3009
+    Epoch [1/3], Step [15/25], Loss: 2.2994
+    Epoch [1/3], Step [20/25], Loss: 2.2982
+    Epoch [1/3], Step [25/25], Loss: 2.2977
+    Epoch [2/3], Step [5/25], Loss: 2.2943
+    Epoch [2/3], Step [10/25], Loss: 2.2945
+    Epoch [2/3], Step [15/25], Loss: 2.2930
+    Epoch [2/3], Step [20/25], Loss: 2.2910
+    Epoch [2/3], Step [25/25], Loss: 2.2916
+    Epoch [3/3], Step [5/25], Loss: 2.2867
+    Epoch [3/3], Step [10/25], Loss: 2.2842
+    Epoch [3/3], Step [15/25], Loss: 2.2886
+    Epoch [3/3], Step [20/25], Loss: 2.2848
+    Epoch [3/3], Step [25/25], Loss: 2.2887
+    """
+    return None
 
 
 def main():
@@ -238,7 +272,8 @@ def main():
                        num_epochs=num_epochs,
                        criterion=criterion,
                        optimizer=optimizer,
-                       device=device)
+                       device=device,
+                       dataset_path=dataset_path)
 
 
 if __name__ == '__main__':
